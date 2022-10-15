2cb35cd4a72a68227cfefac8e72499f49d12686c
Ivan-juni <ivanuk.vanya@gmail.com>
2022-10-15 12:39:44 +0300

    auth validation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const userRouter = require("./userRouter");
+const authRouter = require("./authRouter");
 const gamesRouter = require("./gamesRouter");
 const cartRouter = require("./cartRouter");
 
-router.use("/user", userRouter);
+router.use("/auth", authRouter);
 router.use("/games", gamesRouter);
 router.use("/cart", cartRouter);
 
